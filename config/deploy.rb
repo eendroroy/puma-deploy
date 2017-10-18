a598bfea18943892e933a8409ec6f1b50c2a3aba
indrajit <eendroroy@gmail.com>
2017-10-18 16:24:09 +0600

    fix deploy -> remote:restart after publishing

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,6 +70,6 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
   before 'deploy:setup_config', 'nginx:remove_default_vhost'
   after 'deploy:setup_config', 'nginx:reload'
-  # after 'deploy:setup_config', 'monit:restart'
-  after 'deploy:publishing', 'deploy:restart'
+  after 'deploy:setup_config', 'monit:restart'
+  after 'deploy:publishing', 'remote:restart'
 end
